3ac833e5288e8650d9a56630d94b9282c76a5358
Abbas Roshani <104920213+aroshani1985@users.noreply.github.com>
2022-11-16 18:07:46 +0330

    define structure for receive process flags and its init function.

--- IoTEngCTestPrj/IoTEngCTestPrj/main/com/pktreceive.c
+++ IoTEngCTestPrj/IoTEngCTestPrj/main/com/pktreceive.c
@@ -1,5 +1,15 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "pktreceive.h"
 
 
+struct receive_packet_flags rec_pkt_flags;
 
-
+void init_receive_process_flags(bool FreeMemory)
+{
+	rec_pkt_flags.blok_rx = false;
+	rec_pkt_flags.status = false;
+	rec_pkt_flags.err_code = 0;
+	rec_pkt_flags.free_mem = FreeMemory;
+}
 
--- IoTEngCTestPrj/IoTEngCTestPrj/main/com/pktreceive.h
+++ IoTEngCTestPrj/IoTEngCTestPrj/main/com/pktreceive.h
@@ -1,6 +1,16 @@
 #ifndef _PKTRECEIVE_H
 #define _PKTRECEIVE_H
 
+#include <stdio.h>
 
+// receive processflags
+struct receive_packet_flags
+{
+	uint8_t blok_rx : 1;  // block receive new packet, pervent override receive buffer
+	uint8_t status : 1;   // final status of receive process true for completed and false for incompleted
+	uint8_t free_mem : 1; // free allocated memory inside the receive process or not. to use buffer outside of the receive function
+	uint8_t err_code : 4; // error code of receive process
+};
 
+extern struct receive_packet_flags rec_pkt_flags;
 #endif
